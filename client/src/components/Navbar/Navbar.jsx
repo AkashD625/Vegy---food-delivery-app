854b0c89cdcf2a2f14a89be76d30e328f5b4e3ae
Akash <divateakash21@gmail.com>
2024-08-25 16:09:31 +0530

    navbar-added

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 function Navbar() {
+
+
+      const [menu,setMenu] = useState("home")
   return (
     <div className="navbar">
-    <img src={assets.logo} alt="" className="logo" />
-    <ul className="navbar-menu">
-      <li>Home</li>
-      <li>menu</li>
-      <li>mobile-app</li>
-      <li>Contact us</li>
-    </ul>
-    <div className="navbar-right">
-      <img src={assets.search_icon} alt="" />
-      <div className="dot"></div>
-    </div>
-    <button>Sign in</button>
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li onClick={()=>setMenu("home")}className={menu==="home"?"activi":""}>Home</li>
+        <li onClick={()=>setMenu("menu")}className={menu==="menu"?"activi":""}>menu</li>
+        <li onClick={()=>setMenu("mobile-app")}className={menu==="mobile-app"?"activi":""}>mobile-app</li>
+        <li onClick={()=>setMenu("contact-us")}className={menu==="contact-us"?"activi":""}>Contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign in</button>
+      </div>
     </div>
   );
 }
---
+++ client/src/pages/Placeorder/Placeoder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Placeceoder'
+function Placeoder() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Placeoder;
