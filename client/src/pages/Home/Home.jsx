e0d9c95fab9958c9ae1d182b7449316a3d801133
Akash <divateakash21@gmail.com>
2024-08-27 19:25:08 +0530

    #

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Navbar/Header/Header';
 import ExploreMenu from '../../components/Navbar/ExploreMenu/ExploreMenu';
 
 function Home() {
+
+
+  const [category,setCategory] = useState("All");
   return (
    <>
    <Header/>
-   <ExploreMenu/>
+   <ExploreMenu category={category} setCategory={setCategory}/>
    </>
   )
 }
