2fad3fd451fc42d25919b13aa7b71a8e3d3f2567
Akash <divateakash21@gmail.com>
2024-09-02 16:57:17 +0530

    cart-page

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,31 +2,38 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
-function Navbar({setShowLogin}) {
+function Navbar({ setShowLogin }) {
   const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo" />
+      <Link to="/">
+        <img src={assets.logo} alt="" className="logo" />
+      </Link>
+
       <ul className="navbar-menu">
-        <Link to='/'
+        <Link
+          to="/"
           onClick={() => setMenu("home")}
           className={menu === "home" ? "activi" : ""}
         >
           Home
         </Link>
-        <a href="#explore-menu"
+        <a
+          href="#explore-menu"
           onClick={() => setMenu("menu")}
           className={menu === "menu" ? "activi" : ""}
         >
           menu
         </a>
-        <a href="#app-download"
+        <a
+          href="#app-download"
           onClick={() => setMenu("mobile-app")}
           className={menu === "mobile-app" ? "activi" : ""}
         >
           mobile-app
         </a>
-        <a href='#footer'
+        <a
+          href="#footer"
           onClick={() => setMenu("contact-us")}
           className={menu === "contact-us" ? "activi" : ""}
         >
@@ -36,10 +43,13 @@ function Navbar({setShowLogin}) {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <Link to="/Cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+
           <div className="dot"></div>
         </div>
-        <button onClick={()=>setShowLogin(true)}>Sign in</button>
+        <button onClick={() => setShowLogin(true)}>Sign in</button>
       </div>
     </div>
   );
