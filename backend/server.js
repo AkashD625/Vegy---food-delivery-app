1d4658bae6ec84148560198eba642f79739952f9
Akash <divateakash21@gmail.com>
2024-09-14 19:36:10 +0530

    #

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import { connectDB } from './config/db.js';
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 
 // App config
 const app = express();
@@ -13,15 +14,18 @@ app.use(cors());
 // DB connection
 connectDB();
 
+//api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images",express.static('uploads'))
+
 // Routes
-app.get('/', (req, res) => {
-  res.send('API Working');
+app.get("/", (req, res) => {
+  res.send("API Working");
 });
 
 // Start server
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
-
 
 //mongodb+srv://akashgd:<db_password>@cluster0.jpwku.mongodb.net/?
