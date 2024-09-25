de3d7b192973edb224091a05bb84c84c471d53ac
Akash Divate <143575891+AkashD625@users.noreply.github.com>
2024-09-25 21:58:59 +0530

    Update orderControler.js

--- backend/controlers/orderControler.js
+++ backend/controlers/orderControler.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 //placing user order from forontend
 const placeOrder = async(req,res)=>{
-      const frontend_url = 'http://localhost:3000';
+      const frontend_url = 'https://vegy-admin.onrender.com';
        // or the correct port
         
 
