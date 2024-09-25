7e4c30249ade0341cf81f5a8a3aa40fb69aea932
Akash <divateakash21@gmail.com>
2024-09-25 21:09:48 +0530

    #

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,21 +1,18 @@
-import logo from './logo.png'
-import add_icon from './add_icon.png'
-import order_icon from './order_icon.png'
-import profile_image from './profile_image.png'
-import upload_area from './upload_area.png'
-import parcel_icon from './parcel_icon.png'
+import logo from './logo.png';
+import add_icon from './add_icon.png';
+import order_icon from './order_icon.png';
+import profile_image from './profile_image.png';
+import upload_area from './upload_area.png';
+import parcel_icon from './parcel_icon.png';
 
-export const assets ={
+export const assets = {
     logo,
     add_icon,
     order_icon,
     profile_image,
     upload_area,
     parcel_icon
-}
+};
 
-<<<<<<< HEAD
-export  const url = "https://vegy-food-delivery-web.onrender.com";
-=======
+// Ensure only one url is exported
 export const url = "https://vegy-food-delivery-web.onrender.com";
->>>>>>> a31b7359deebaac32474ca719b3781560b1dd172
